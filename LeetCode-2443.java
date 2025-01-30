2084a3b7fbb33122fe058ea16685b5775dd821f6
Nikhil Shimpy <nikhilshimpy712@gmail.com>
2025-01-30 22:28:41 +0530

    Update LeetCode-2443.java

--- LeetCode-2443.java
+++ LeetCode-2443.java
@@ -1,3 +1,4 @@
+//APPROACH 1
 class Solution {
     public boolean sumOfNumberAndReverse(int num) {
         if(num == 0) return true;
@@ -10,3 +11,26 @@ class Solution {
         return false;
     }
 }
+
+//APPROACH 2
+//OPTIMIZED
+class Solution {
+    public boolean sumOfNumberAndReverse(int num) {
+        if (num == 0) return true;
+
+        for (int i = num / 2; i <= num; i++) { // Start from num/2
+            if (i + reverse(i) == num) return true;
+        }
+        return false;
+    }
+
+    private int reverse(int n) {
+        int rev = 0;
+        while (n > 0) {
+            rev = rev * 10 + n % 10;  // Extract last digit and append to rev
+            n /= 10;  // Remove last digit from n
+        }
+        return rev;
+    }
+}
+
