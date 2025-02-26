699e1ed49973be49b2a43d9238f8572e431fd25b
Nikhil Shimpy <nikhilshimpy712@gmail.com>
2025-02-26 23:07:51 +0530

    Create LeetCode-162.java
    A peak element is an element that is strictly greater than its neighbors.
    
    Given a 0-indexed integer array nums, find a peak element, and return its index. If the array contains multiple peaks, return the index to any of the peaks.
    
    You may imagine that nums[-1] = nums[n] = -∞. In other words, an element is always considered to be strictly greater than a neighbor that is outside the array.
    
    You must write an algorithm that runs in O(log n) time.
    
     
    
    Example 1:
    
    Input: nums = [1,2,3,1]
    Output: 2
    Explanation: 3 is a peak element and your function should return the index number 2.
    Example 2:
    
    Input: nums = [1,2,1,3,5,6,4]
    Output: 5
    Explanation: Your function can return either index number 1 where the peak element is 2, or index number 5 where the peak element is 6.
     
    
    Constraints:
    
    1 <= nums.length <= 1000
    -231 <= nums[i] <= 231 - 1
    nums[i] != nums[i + 1] for all valid i.

---
+++ LeetCode-162.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+       int n = nums.length;
+       int s = 0;
+       int e  = n-1;
+        if(n == 1) return 0;
+        if(nums[0] > nums[1]) return 0;
+        if(nums[n-1] > nums[n-2]) return n-1;
+
+       while(s<=e){
+        int mid = s + (e-s)/2;
+        if(mid!=0 && mid != n-1 && nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1] ) return mid;
+        else if(mid!=n-1 && nums[mid]<nums[mid+1]) s = mid+1;
+        else e = mid-1;
+
+       } 
+       return -1;
+    }
+}
