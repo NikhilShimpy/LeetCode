335596b9b87d0b544307747cacab2707deef12e1
Nikhil Shimpy <nikhilshimpy712@gmail.com>
2025-08-30 23:28:54 +0530

    Create LeetCode-36.java

---
+++ LeetCode-36.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        boolean[][] row = new boolean[9][9];
+        boolean[][] col = new boolean[9][9];
+        boolean[][] box = new boolean[9][9];
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+
+                int digit = board[i][j] - '0' - 1;
+                int boxindex = (i / 3) * 3 + (j / 3); 
+
+                if (row[i][digit] || col[j][digit] || box[boxindex][digit]) {
+                    return false;
+                }
+
+                row[i][digit] = true;
+                col[j][digit] = true;
+                box[boxindex][digit] = true;
+            }
+        }
+        return true;
+    }
+}
