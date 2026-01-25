7d1432d06e8bbb0ff66d7e3804d611f2d8fec2b1
Nikhil Shimpy <nikhilshimpy712@gmail.com>
2026-01-26 01:45:11 +0530

    Create LeetCode-1984.java

---
+++ LeetCode-1984.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        
+        if(k <= 1) return 0;
+        int n = nums.length;
+        Arrays.sort(nums);
+        int min = Integer.MAX_VALUE;
+        for(int i =0;i<=n-k;i++){
+            min = Math.min(min,nums[i+k-1]-nums[i]);
+        }
+        return min;
+    }
+}
