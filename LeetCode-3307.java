b9f56a846b9aa31eebfe085c6dbd91d3aeef1b38
Nikhil Shimpy <nikhilshimpy712@gmail.com>
2026-04-05 01:55:00 +0530

    Refactor kthCharacter method for clarity and efficiency

--- LeetCode-3307.java
+++ LeetCode-3307.java
@@ -1,18 +1,55 @@
+// RECURSION
+
 class Solution {
     public char kthCharacter(long k, int[] operations) {
-        int i = (int) (Math.ceil(Math.log(k) / Math.log(2))) - 1;
-        int count = 0;
-
-        while (k > 1) {
-            if (k > (1L << i)) {
-                if (operations[i] == 1) {
-                    count++;
-                }
-                k -= (1L << i);
-            }
-            i--;
+       
+        if (k == 1) {
+            return 'a';
         }
 
-        return (char) ('a' + (count % 26));
+        int n = operations.length;
+        int optype = 0; 
+        long len = 1;
+        long newk = k;
+
+        for (int i = 0; i < n; i++) {
+            len *= 2;
+            if (len >= k) { 
+                optype = operations[i];
+                newk = k - len / 2; 
+                break;
+            }
+        }
+
+        char res = kthCharacter(newk, operations);
+
+        if (optype == 0) 
+            return res;
+
+        if (res == 'z') 
+            return 'a';
+
+        return (char) (res + 1); 
     }
 }
+
+
+//bit man.
+// class Solution {
+//     public char kthCharacter(long k, int[] operations) {
+//         int i = (int) (Math.ceil(Math.log(k) / Math.log(2))) - 1;
+//         int count = 0;
+
+//         while (k > 1) {
+//             if (k > (1L << i)) {
+//                 if (operations[i] == 1) {
+//                     count++;
+//                 }
+//                 k -= (1L << i);
+//             }
+//             i--;
+//         }
+
+//         return (char) ('a' + (count % 26));
+//     }
+// }
