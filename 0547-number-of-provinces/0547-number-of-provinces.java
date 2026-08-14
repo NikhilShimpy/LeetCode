f2442a4aaa205dd307022e1f39621be873549abd
Nikhil Shimpy <nikhilshimpy712@gmail.com>
2026-08-14 18:06:22 +0530

    Time: 4 ms (15.52%), Space: 49.1 MB (19.6%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,7 +1,10 @@
+
+//bfs
 class Solution {
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
         //create adj list
         for(int i=1;i<=n;i++){
             adj.add(new ArrayList<>());
@@ -16,22 +19,67 @@ class Solution {
             }
         }
         boolean[] visited = new boolean[n];
-        int count = 0;
-
+        int count= 0;
         for(int i=0;i<n;i++){
             if(!visited[i]){
-                dfs(i,adj,visited);
+                bfs(i,adj,visited);
                 count++;
             }
         }
-        return count;
+    return count;
     }
-    public void dfs(int node,ArrayList<ArrayList<Integer>> adj, boolean[] visited){
-        visited[node] = true;
-        for(int next : adj.get(node)){
-            if(!visited[next]){
-                dfs(next,adj,visited);
+    public void bfs(int node,ArrayList<ArrayList<Integer>> adj , boolean[] visited ){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(node);
+        visited[node]=true;
+
+        while(!queue.isEmpty()){
+            int curr = queue.poll();
+            for(int next : adj.get(curr)){
+                if(!visited[next]){
+                    queue.add(next);
+                    visited[next] = true;
+                }
             }
         }
     }
 }
+
+//DFS
+// class Solution {
+//     public int findCircleNum(int[][] isConnected) {
+//         int n = isConnected.length;
+//         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+//         //create adj list
+//         for(int i=1;i<=n;i++){
+//             adj.add(new ArrayList<>());
+//         }
+//         //build graph
+//         for(int i=0;i<n;i++){
+//             for(int j =0;j<n;j++){
+//                 if(isConnected[i][j] == 1){
+//                     adj.get(i).add(j);
+//                     adj.get(j).add(i);
+//                 }
+//             }
+//         }
+//         boolean[] visited = new boolean[n];
+//         int count = 0;
+
+//         for(int i=0;i<n;i++){
+//             if(!visited[i]){
+//                 dfs(i,adj,visited);
+//                 count++;
+//             }
+//         }
+//         return count;
+//     }
+//     public void dfs(int node,ArrayList<ArrayList<Integer>> adj, boolean[] visited){
+//         visited[node] = true;
+//         for(int next : adj.get(node)){
+//             if(!visited[next]){
+//                 dfs(next,adj,visited);
+//             }
+//         }
+//     }
+// }
